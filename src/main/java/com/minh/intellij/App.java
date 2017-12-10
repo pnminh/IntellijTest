90ec8265c1de1bfa60a655f43bac9ed252b3aa64
Minh Pham <pnminh232@gmail.com>
2017-12-09 20:43:52 -0600

    init file

---
+++ src/main/java/com/minh/intellij/App.java
@@ -0,0 +1,13 @@
+package com.minh.intellij;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
